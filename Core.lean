20f0bca8d52b3c0cf1663f3a1cac0bd3e4b03ef3
Yukiya Suzuki <suzuki.kakuritsu@gmail.com>
2026-04-18 06:42:42 +0900

    Update Core.lean

--- Core.lean
+++ Core.lean
@@ -1,9 +1,12 @@
-namespace ABC
-
 structure Triple where
   a : Nat
   b : Nat
   c : Nat
+  pos_a : 0 < a
+  pos_b : 0 < b
+  pos_c : 0 < c
   sum : a + b = c
+  coprime : Nat.gcd a b = 1
 
-end ABC
+def embed (t : Triple) : Nat × Nat × Nat :=
+  (t.a, t.b, t.c)
